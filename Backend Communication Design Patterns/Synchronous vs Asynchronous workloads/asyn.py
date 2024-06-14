93ff59bd229555ca5da02b98d7c4bc071a96613f
Anshu Kumar <95365748+anshu049@users.noreply.github.com>
2024-06-15 03:49:15 +0530

    Update asyn.py

--- Backend Communication Design Patterns/Synchronous vs Asynchronous workloads/asyn.py
+++ Backend Communication Design Patterns/Synchronous vs Asynchronous workloads/asyn.py
@@ -1,20 +1,22 @@
 import asyncio
 import aiofiles
 
+async def read_file_async(file_path):
+    async with aiofiles.open(file_path, 'r') as file:
+        return await file.read()
+
 async def main():
     # Print the first log message
     print("1\n")
 
     # Read the file asynchronously
-    async with aiofiles.open("test.txt", "r") as file:
-        res = await file.read()
-
-    # Print the file contents with a newline after
-    print(f"file: {res}\n")
+    file_content = await read_file_async("test.txt")
 
     # Print the second log message
     print("2")
 
-# Run the async main function
-asyncio.run(main())
+    # Print the file contents with a newline after
+    print(f"file: {file_content}\n")
 
+# Run the main function
+asyncio.run(main())
